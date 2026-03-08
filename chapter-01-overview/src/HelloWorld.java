d44099a0916c73d455f2ed59acc19edacae65788
Yu <1521260493@qq.com>
2026-03-08 17:34:35 +0800

    HelloWorld入门案例

---
+++ chapter-01-overview/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * 入门案例
+ *
+ * @author Yu
+ * @create 2026-03-08 17:30
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
