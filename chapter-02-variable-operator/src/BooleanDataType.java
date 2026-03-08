e63297d49417c35adf3fff38a8c05c062e7fe1b2
Yu <1521260493@qq.com>
2026-03-08 21:53:19 +0800

    布尔类型的使用

---
+++ chapter-02-variable-operator/src/BooleanDataType.java
@@ -0,0 +1,23 @@
+/**
+ * 布尔类型
+ *
+ * @author Yu
+ * @create 2026-03-08 21:51
+ */
+public class BooleanDataType {
+    public static void main(String[] args) {
+        boolean isMarry = true;
+        isMarry = false;
+
+        // 错误的
+        //isMarry = 0;
+        //isMarry = "是";
+
+        boolean isFlag = true;
+        if(isFlag){
+            //true分支
+        }else{
+            //false分支
+        }
+    }
+}
