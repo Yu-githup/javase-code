5396ab0401a139b6b7e5c11017200952c99939a9
Yu <1521260493@qq.com>
2026-03-08 21:00:58 +0800

    标识符的命名规范

---
+++ chapter-02-variable-operator/src/HelloWorld.java
@@ -0,0 +1,29 @@
+/**
+ * 标识符的命名规范‘
+ *
+ * @author Yu
+ * @create 2026-03-08 20:57
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        char age;
+        String name;
+        String bookName;
+    }
+
+    public static int[] binarySearch() {
+        return null;
+    }
+
+    public String getName() {
+        return "";
+    }
+}
+
+interface UsbInterface {
+    int  MAX_VALUE = 1;
+    int  PI = 1;
+    final int  DEFAULT_CAPACITY = 0;
+}
+
+//class System {}
