06767410fc8f50eb3d9408ec19e375d46ad99810
Yu <1521260493@qq.com>
2026-03-08 22:54:57 +0800

    比较运算符

---
+++ chapter-02-variable-operator/src/CompareTest.java
@@ -0,0 +1,26 @@
+/**
+ * 比较运算符
+ *
+ * @author Yu
+ * @create 2026-03-08 22:50
+ */
+public class CompareTest {
+    public static void main(String[] args) {
+        int i1 = 10;
+        int i2 = 20;
+
+        System.out.println(i1 == i2);// false
+        System.out.println(i1 != i2);// true
+        System.out.println(i1 >= i2);// false
+
+        int m = 10;
+        int n = 20;
+        System.out.println(m == n);// false
+        System.out.println(m = n);// 20
+
+        boolean b1 = false;
+        boolean b2 = true;
+        System.out.println(b1 == b2);// false
+        System.out.println(b1 = b2);// true
+    }
+}
