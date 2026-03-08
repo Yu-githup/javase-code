45c77178f456140b907b7dddff33cf9404667fbe
Yu <1521260493@qq.com>
2026-03-08 18:00:54 +0800

    章节案例

---
+++ chapter-01-overview/src/PersonalInfo.java
@@ -0,0 +1,15 @@
+/**
+ * 章节案例：输出个人信息
+ *
+ * @author Yu
+ * @create 2026-03-08 17:59
+ */
+public class PersonalInfo {
+    public static void main(String[] args) {
+        System.out.println("姓名：张三");
+        // 换行的操作
+        System.out.println();
+        System.out.println("性别：男");
+        System.out.println("家庭住址：北京程序员聚集地：中关村");
+    }
+}
---
+++ chapter-01-overview/src/StarPrintTestV1.java
@@ -0,0 +1,99 @@
+/**
+ * 章节案例：输出心形
+ *
+ * @author Yu
+ * @create 2026-03-08 17:59
+ */
+public class StarPrintTestV1 {
+    public static void main(String[] args) {
+        System.out.print("\t");
+        System.out.print("*");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+
+        System.out.println("*");
+
+
+        System.out.print("*");
+        System.out.print("\t");
+        //System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("I love java");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.println("*");
+
+
+        System.out.print("\t");
+        System.out.print("*");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+
+        System.out.println("*");
+
+
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("*");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+
+        System.out.println("*");
+
+
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("*");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+
+        System.out.println("*");
+
+
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("*");
+        System.out.print("\t");
+
+        System.out.println("*");
+
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("\t");
+        System.out.print("    ");
+        System.out.print("*");
+
+        // 方式2
+        System.out.println();
+        System.out.print("\t"+"*"+"\t\t\t\t\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");
+        System.out.print("*"+"\t\t"+"*"+"\t\t\t\t"+"I love Java"+"\t\t\t\t"+"*"+"\t\t"+"*"+"\n");
+        System.out.print("\t"+"*"+"\t\t\t\t\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");
+        System.out.print("\t\t"+"*"+"\t\t\t\t\t\t\t\t\t\t"+"*"+"\t\t"+"\n");
+        System.out.print("\t\t\t"+"*"+"\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");
+        System.out.print("\t\t\t\t"+"*"+"\t\t\t\t\t\t"+"*"+""+"\t"+"\n");
+        System.out.print("\t\t\t\t\t"+"*"+"\t\t\t\t"+"*"+""+"\t\t"+"\n");
+        System.out.print("\t\t\t\t\t\t"+"*"+"\t\t"+"*"+""+"\t\t"+"\n");
+        System.out.print("\t\t\t\t\t\t\t"+"*"+"\n");
+    }
+}
